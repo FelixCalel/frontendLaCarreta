3ca3c3f0358465d8c292811faa38245c24a36d81
FelixCalel <felixcalel806@gmail.com>
2024-09-12 09:27:18 -0600

    Deu y correlativo

--- src/pages/tienda/componentes/DeuSelector.jsx
+++ src/pages/tienda/componentes/DeuSelector.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Flex, FormHelperText,  } from "@chakra-ui/react";
+import { Flex, FormHelperText } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   AutoComplete,
@@ -9,8 +9,8 @@ import {
 } from "@choc-ui/chakra-autocomplete";
 
 import { tablaDeudores } from "../../../store/Deus/thunks";
- 
- const DeuSelector = () => {
+
+const DeuSelector = () => {
   const dispatch = useDispatch();
   const deus = useSelector((state) => state.deudores);
 
@@ -19,26 +19,24 @@ import { tablaDeudores } from "../../../store/Deus/thunks";
   }, [dispatch]);
 
   return (
-    <Flex pt="4" justify="start" align="center" w="full" flexDir="column"> {/* Cambié justify a 'start' y reduje padding top */}
+    <Flex pt="4" justify="start" align="center" w="full" flexDir="column">
       <AutoComplete openOnFocus>
-        <AutoCompleteInput variant="outline" placeholder="Seleccione un deudor" /> {/* Agregué un placeholder */}
+        <AutoCompleteInput variant="outline" placeholder="Seleccione un deudor" />
         <AutoCompleteList>
           {deus.deudores.map((deu) => (
             <AutoCompleteItem
               key={`option-${deu.id}`}
-              value={deu.nombre}
+              value={`${deu.nombre} - ${deu.correlativo}`}
               textTransform="capitalize"
             >
-              {deu.nombre}
+              {`${deu.nombre} - ${deu.correlativo}`}
             </AutoCompleteItem>
           ))}
         </AutoCompleteList>
       </AutoComplete>
-      <FormHelperText mt="2">Seleccione el deudor para esta tienda</FormHelperText> {/* Cambié el texto para que sea más claro */}
+      <FormHelperText mt="2">Seleccione el deudor para esta tienda</FormHelperText>
     </Flex>
   );
 };
-
-
 
 export default DeuSelector;
