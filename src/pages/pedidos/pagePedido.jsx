72c23fe2170f5404f024060b60ad58b0b5e70513
FelixCalel <felixcalel806@gmail.com>
2024-11-08 10:28:18 -0600

    sin error

--- src/pages/pedidos/pagePedido.jsx
+++ src/pages/pedidos/pagePedido.jsx
@@ -1,13 +1,13 @@
 import { Box } from "@chakra-ui/react";
-import PageFormPedidos from "./pageFormPedidos"; // Importación correcta de la exportación por defecto
+import DetallePedidoForm from "./pageFormPedidos"; // Importación correcta de la exportación por defecto
 
 export const PageDetallePedido = () => {
   return (
     <Box p={4} display="flex" justifyContent="center" alignItems="center">
       {/* Aquí puedes ajustar el componente principal del formulario */}
-      <PageFormPedidos /> {/* Uso correcto del componente con letra mayúscula */}
+      <DetallePedidoForm /> {/* Uso correcto del componente con letra mayúscula */}
     </Box>
   );
 };
 
-export default PageDetallePedido;
+export default DetallePedidoForm;
