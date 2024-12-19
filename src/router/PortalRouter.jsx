2c311a8c110a320308621998c3bd1a45f49e8ae4
FelixCalel <felixcalel806@gmail.com>
2024-12-19 15:09:50 -0600

    cambios

--- src/router/PortalRouter.jsx
+++ src/router/PortalRouter.jsx
@@ -3,7 +3,6 @@ import { RootLayout } from "../pages/layouts/RootLayout";
 import { PagePerfil,Dashboard, PageEmpresa, PageUsuarios, PageRoles, PageOrdenes, PageFacturas } from "../pages";
 import { Logout } from "../pages/auth/Logout";
 import { PagePermisos } from "../pages/PagePermisos";
-
 import { PageListarUsuarios } from "../pages/Usuarios/PageListarUsuarios.jsx";
 import { PageModulos } from "../pages/Modulos/PageModulos";
 import { PageOpciones } from "../pages/Opciones/PageOpciones";
@@ -18,20 +17,18 @@ import AprobadosPage from "../pages/pedidos/paginaExportacion/pageFormExportacio
 export const PortalRouter = () => {
   return (
     <Routes>
-      {/* <Route path="/*" element={<Dashboard />} > */}
+
       <Route path="/*" element={<RootLayout />}>
+      
         <Route index element={<Dashboard />} />
-        {/* <Route path="dashboard" element={<Dashboard />} />  */}
         <Route path="perfil" element={<PagePerfil />} />
         <Route path="empresas" element={<PageEmpresa />} />
         <Route path="usuarios" element={<PageUsuarios />} />
-        
         <Route path="roles" element={<PageRoles />} />
         <Route path="permisos" element={<PagePermisos />} />
         <Route path="ordenes" element={<PageOrdenes />} />
         <Route path="facturas" element={<PageFacturas />} />
         <Route path="logout" element={<Logout /> }></Route>
-
         <Route path="listarUsuarios" element={<PageListarUsuarios />}> </Route>
         <Route path="Modulos/listarModulos" element={<PageModulos />} > </Route>
         <Route path="listarOpciones" element={<PageOpciones />} > </Route>
@@ -41,11 +38,8 @@ export const PortalRouter = () => {
         <Route path="listarasignacionMO" element={<PageasignacionMO />} > </Route>
         <Route path="listarDeus" element={<PageFormDeus />} > </Route>
         <Route path="exportarPedidos" element={< AprobadosPage/>} > </Route>
-    
 
-        {/* Más rutas anidadas si es necesario */}
       </Route>
-      {/* </Route>  */}
     </Routes>
   );
 }
