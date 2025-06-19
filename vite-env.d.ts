f3bd9c9888cbcd5644eabc417ceb443e7d19272c
FelixCalel <felixcalel806@gmail.com>
2025-06-19 08:30:55 -0600

    script start agregado

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,6 +1,8 @@
 interface ImportMetaEnv {
     readonly VITE_API_URL: string;
     readonly VITE_WEBSOCKET_URL: string;
+    readonly HOST: string;
+    readonly PORT: string;
 }
 
 interface ImportMeta {
