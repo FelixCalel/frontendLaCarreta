d77db25eee6ec2f95d6e4de6232e1ac40fc0cd1f
FelixCalel <felixcalel806@gmail.com>
2025-07-18 14:27:44 -0600

    Orden de los detalles realizado mas diseño de comentario y fecha con el buscador

--- src/pages/pedidos/pedidosEntrantes/pageFormPedidosEntrantes.jsx
+++ src/pages/pedidos/pedidosEntrantes/pageFormPedidosEntrantes.jsx
@@ -31,8 +31,6 @@ import ApproveOrderDialog from "../componentes/EntrantesFormPedidos/ApproveOrder
 import { selectPedidosEntrantesPorRuta } from "./componentes/rutaSelectors";
 import { tablaTienda } from "../../../store/Tienda/thunks";
 import { useSearch } from "../../../components/component/SearchContext";
-import CancelOrderDialog from "./componentes/CancelOrderDialog";
-
 //import { format } from "date-fns";
 
 const EntrantesPage = () => {
