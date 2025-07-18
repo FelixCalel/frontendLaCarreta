c3067d1df8d232ce53d575c6c60ef89898384161
FelixCalel <felixcalel806@gmail.com>
2025-07-18 16:18:44 -0600

    aahh

--- src/pages/pedidos/componentes/EntrantesFormPedidos/detallesModal.jsx
+++ src/pages/pedidos/componentes/EntrantesFormPedidos/detallesModal.jsx
@@ -26,7 +26,6 @@ import {
 import { FaCalendarAlt, FaCommentDots, FaBoxOpen } from "react-icons/fa";
 
 const DetallesModal = ({ isOpen, onClose, detalles = [], pedido = null }) => {
-  /* ---------  tokens de color / estilos --------- */
   const bg = useColorModeValue("white", "gray.800");
   const borderColor = useColorModeValue("gray.200", "gray.600");
   const rowHoverBg = useColorModeValue("gray.50", "gray.700");
@@ -170,7 +169,6 @@ const DetallesModal = ({ isOpen, onClose, detalles = [], pedido = null }) => {
   );
 };
 
-/* ----------  PropTypes  ---------- */
 DetallesModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
