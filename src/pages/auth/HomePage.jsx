a038b30ade0494b4725bb81ee9aa83631c3f6243
FelixCalel <felixcalel806@gmail.com>
2025-08-27 09:33:03 -0600

    prueba dugui

--- src/pages/auth/HomePage.jsx
+++ src/pages/auth/HomePage.jsx
@@ -122,7 +122,7 @@ const HomePage = () => {
               >
                 <VStack align="stretch" spacing={2}>
                   <Heading fontSize={{ base: "2xl", md: "3xl" }}>
-                    Bienvenido {nombreUsuario}
+                    Bienvenuti {nombreUsuario}
                     {rolNombre !== "Sin rol" && (
                       <Badge
                         ml={2}
