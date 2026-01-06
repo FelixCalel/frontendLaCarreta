c683fadd78242ebe2e712e939df8ef65d1af9fa4
FelixCalel <104239279+FelixCalel@users.noreply.github.com>
2026-01-06 09:37:34 -0600

    Orden ascendente segun las rutas

--- src/pages/tienda/componentes/Filtros.jsx
+++ src/pages/tienda/componentes/Filtros.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'; // Importamos PropTypes
+import PropTypes from 'prop-types';
 import { Box, FormControl, FormLabel, Select, Input } from '@chakra-ui/react';
 
 const Filtros = ({
@@ -10,13 +10,11 @@ const Filtros = ({
     setFiltroZona,
     data,
   }) => {
-    // Filtrar datos únicos para Ciudad y Ruta
     const uniqueCiudades = [...new Set(data.map((tienda) => tienda.nombreCiudad))];
     const uniqueRutas = [...new Set(data.map((tienda) => tienda.nombreRuta))];
   
     return (
       <Box mb="20px" display="flex" gap={4}>
-        {/* Filtro Ciudad */}
         <FormControl>
           <FormLabel>Ciudad</FormLabel>
           <Select
@@ -32,7 +30,6 @@ const Filtros = ({
           </Select>
         </FormControl>
   
-        {/* Filtro Ruta */}
         <FormControl>
           <FormLabel>Ruta</FormLabel>
           <Select
@@ -48,7 +45,6 @@ const Filtros = ({
           </Select>
         </FormControl>
   
-        {/* Filtro Zona */}
         <FormControl>
           <FormLabel>Zona</FormLabel>
           <Input
@@ -61,15 +57,14 @@ const Filtros = ({
     );
   };
 
-// Agregar validaciones de las props usando PropTypes
 Filtros.propTypes = {
-  filtroCiudad: PropTypes.string.isRequired,      // filtroCiudad debe ser un string y es requerido
-  setFiltroCiudad: PropTypes.func.isRequired,     // setFiltroCiudad debe ser una función y es requerido
-  filtroRuta: PropTypes.string.isRequired,        // filtroRuta debe ser un string y es requerido
-  setFiltroRuta: PropTypes.func.isRequired,       // setFiltroRuta debe ser una función y es requerido
-  filtroZona: PropTypes.string.isRequired,        // filtroZona debe ser un string y es requerido
-  setFiltroZona: PropTypes.func.isRequired,       // setFiltroZona debe ser una función y es requerido
-  data: PropTypes.array.isRequired,               // data debe ser un array y es requerido
+  filtroCiudad: PropTypes.string.isRequired,
+  setFiltroCiudad: PropTypes.func.isRequired,
+  filtroRuta: PropTypes.string.isRequired,
+  setFiltroRuta: PropTypes.func.isRequired,
+  filtroZona: PropTypes.string.isRequired,
+  setFiltroZona: PropTypes.func.isRequired,
+  data: PropTypes.array.isRequired,
 };
 
 export default Filtros;
