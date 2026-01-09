34acdffe0f0267ad45628c707958d86a1a2ef82f
FelixCalel <104239279+FelixCalel@users.noreply.github.com>
2026-01-09 10:00:09 -0600

    listo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hd3b0g3me0c"
+    "revision": "0.0sp1b4c6epg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
