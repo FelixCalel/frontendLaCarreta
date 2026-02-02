f95990857b1c593e496be26685edc238c4041aec
FelixCalel <104239279+FelixCalel@users.noreply.github.com>
2026-02-02 12:10:40 -0600

    fecha lista

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qnin81d1aso"
+    "revision": "0.t24ni7kr29s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
